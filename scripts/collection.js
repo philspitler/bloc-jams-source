5259a6538d716fb402c0d8382d832ad2837b8546
Phil Spitler (PRS) <phil@bloc.io>
2016-09-14 18:20:31 -0400

    checkpoint 10 complete

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,3 +1,29 @@
 BlocJams.collection = (function() {
+	var collectionItemTemplate = function(name, artist, albumArtUrl, songCount) {
+		return '<div class="collection-album-container column fourth">' +
+			'  <img src="' + albumArtUrl + '"/>' +
+			'  <div class="collection-album-info caption">' +
+			'    <p>' +
+			'      <a class="album-name" href="album.html?album=' + name + '">' + name + '</a>' +
+			'      <br/>' +
+			'      <a href="album.html">' + artist + '</a>' +
+			'      <br/>' +
+			'      ' + songCount + ' songs' +
+			'      <br/>' +
+			'    </p>' +
+			'  </div>' +
+			'</div>';
+	}
 
+	var loadCollection = function(albums, collectionContainer) {
+		collectionContainer.innerHTML = '';
+		for (key in albums) {
+			var album = albums[key];
+			collectionContainer.innerHTML += collectionItemTemplate(album.name, album.artist, album.albumArtUrl, album.songs.length);
+		}
+	};
+
+	return {
+		loadCollection: loadCollection
+	}
 }());
