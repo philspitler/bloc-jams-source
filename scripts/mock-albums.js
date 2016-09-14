34b823910bbdbaedf011b69e2631ca382b1a25b3
Phil Spitler (PRS) <phil@bloc.io>
2016-09-14 19:56:59 -0400

    fixed some tabbing

--- scripts/mock-albums.js
+++ scripts/mock-albums.js
@@ -1,43 +1,43 @@
 BlocJams.albums = (function() {
-   var allAlbums = [
-    {
-      name: 'The Colors',
-      artist: 'Pablo Picasso',
-      label: 'Cubism',
-      year: '1881',
-      albumArtUrl: 'assets/images/album_covers/01.png',
-      songs: [
-        { name: 'Blue', length: '4:26' },
-        { name: 'Green', length: '3:14' },
-        { name: 'Red', length: '5:01' },
-        { name: 'Pink', length: '3:21' },
-        { name: 'Magenta', length: '2:15' }
-      ]
-    },
-    {
-      name: 'The Telephone',
-      artist: 'Guglielmo Marconi',
-      label: 'EM',
-      year: '1909',
-      albumArtUrl: 'assets/images/album_covers/20.png',
-      songs: [
-        { name: 'Hello, Operator?', length: '1:01' },
-        { name: 'Ring, ring, ring', length: '5:01' },
-        { name: 'Fits in your pocket', length: '3:21' },
-        { name: 'Can you hear me now?', length: '3:14' },
-        { name: 'Wrong phone number', length: '2:15' }
-      ]
+    var allAlbums = [
+        {
+            name: 'The Colors',
+            artist: 'Pablo Picasso',
+            label: 'Cubism',
+            year: '1881',
+            albumArtUrl: 'assets/images/album_covers/01.png',
+            songs: [
+                { name: 'Blue', length: '4:26' },
+                { name: 'Green', length: '3:14' },
+                { name: 'Red', length: '5:01' },
+                { name: 'Pink', length: '3:21' },
+                { name: 'Magenta', length: '2:15' }
+            ]
+        }
+    ,
+        {
+            name: 'The Telephone',
+            artist: 'Guglielmo Marconi',
+            label: 'EM',
+            year: '1909',
+            albumArtUrl: 'assets/images/album_covers/20.png',
+            songs: [
+                { name: 'Hello, Operator?', length: '1:01' },
+                { name: 'Ring, ring, ring', length: '5:01' },
+                { name: 'Fits in your pocket', length: '3:21' },
+                { name: 'Can you hear me now?', length: '3:14' },
+                { name: 'Wrong phone number', length: '2:15' }
+            ]
+        }
+    ];
+    return {
+    	getAll: function() {
+    		return allAlbums;
+    	},
+    	getOne: function(albumName) {
+    		return allAlbums.filter(function(element) {
+    			return element.name === albumName;
+    		})[0];
+    	}
     }
-  ];
-
-  return {
-    getAll: function() {
-      return allAlbums;
-    },
-    getOne: function(albumName) {
-      return allAlbums.filter(function(element) {
-        return element.name === albumName;
-      })[0];
-    }
-  }
 }());
